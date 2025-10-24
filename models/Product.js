c08f8302c8f22b0152d771971431ee51e7b9d5be
spideyweb1933-creator <spideyweb1933@gmail.com>
2025-10-25 02:46:54 +0300

    Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,12 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  imageUrl: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model('Product', productSchema);
 
