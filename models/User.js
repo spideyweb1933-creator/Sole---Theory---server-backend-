b4350f1d998dfb778cd39fb5273e6679df428d06
spideyweb1933-creator <spideyweb1933@gmail.com>
2025-10-25 04:30:23 +0300

    User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,12 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  passwordHash: { type: String, required: true },
+  role: { type: String, enum: ['admin', 'editor', 'user'], default: 'admin' },
+  createdAt: { type: Date, default: Date.now }
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
 
